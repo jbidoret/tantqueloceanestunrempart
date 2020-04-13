b1d2f5628069b0ef8e0c413a2bb6c7220d02d95b
Julien Bidoret <julienbidoret@gmail.com>
2020-04-13 13:45:51 +0200

    Correction styles mobile

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -14,11 +14,11 @@
             <div id="mc_embed_signup">
             <form action="https://art.us19.list-manage.com/subscribe/post?u=bdc40aac02547797527403a50&amp;id=1bd66fa6d3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                 <div id="mc_embed_signup_scroll">
-                <h2>La suite dans votre boîte e-mail ? Abonnez-vous :</h2>
+                <h2>La suite dans votre boîte e-mail ?</h2>
                 <div class="mc-field-group">
                     <input placeholder="Adresse e-mail" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_bdc40aac02547797527403a50_1bd66fa6d3" tabindex="-1" value=""></div>
-                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                    <input type="submit" value="Abonnez-vous !" name="subscribe" id="mc-embedded-subscribe" class="button">
                 </div>
                 <div id="mce-responses" class="clear">
                     <div class="response" id="mce-error-response" style="display:none"></div>
