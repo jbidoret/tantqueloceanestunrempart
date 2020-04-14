539cb6983b8bb1728a681f00fce484a88130c584
Julien Bidoret <julienbidoret@gmail.com>
2020-04-14 22:32:30 +0200

    ≈

--- site/snippets/header.metas.php
+++ site/snippets/header.metas.php
@@ -1,23 +1,26 @@
 <title><?= $site->title()->html() . r($page !== $site->homePage(),  ' – ' . $page->title()->html() ) ?></title>
 
-<meta name="description" content="<?php e($page->intro()->isNotEmpty(), $page->intro()->text(), $site->description()->text()) ?>">
+<meta name="description" content="<?php e($page->text()->isNotEmpty(), $page->text()->excerpt(255), "Un texte de Quentin Armand – Design de Julien Bidoret – Maison des éditions.") ?>">
 <meta name="keywords" content="<?= $site->keywords()->text() ?>">
 
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<link rel="apple-touch-icon" sizes="180x180" href="<?= url("assets/favicons/apple-touch-icon.png")?>">
-<link rel="icon" type="image/png" sizes="32x32" href="<?= url("assets/favicons/favicon-32x32.png")?>">
-<link rel="icon" type="image/png" sizes="16x16" href="<?= url("assets/favicons/favicon-16x16.png")?>">
-<link rel="manifest" href="<?= url("assets/favicons/site.webmanifest")?>">
-<link rel="mask-icon" href="<?= url("assets/favicons/safari-pinned-tab.svg")?>" color="#000000">
-<link rel="shortcut icon" href="/favicon.ico">
-<meta name="msapplication-TileColor" content="#b91d47">
-<meta name="msapplication-config" content="<?= url("assets/favicons/browserconfig.xml")?>">
-<meta name="theme-color" content="#ffffff">
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="<?= url('/assets/favicons/apple-touch-icon.png')?>">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= url('/assets/favicons/favicon-32x32.png')?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= url('/assets/favicons/favicon-16x16.png')?>">
+<link rel="manifest" href="<?= url('/assets/favicons/site.webmanifest')?>">
+<link rel="mask-icon" color="#154074" href="<?= url('/assets/favicons/safari-pinned-tab.svg')?>">
+<link rel="shortcut icon" href="<?= url('/assets/favicons/favicon.ico')?>">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-config" content="/assets/favicons/browserconfig.xml">
+<meta name="theme-color" content="#154074">
+
+
 
 <meta property="og:url" content="<?= $site->url() ?>">
 <meta property="og:type" content="website">
 <meta property="og:title" content="<?= r($page !== $site->homePage(), $page->title()->html() . ' | ') . $site->title()->html() ?>">
-<meta property="og:description" content="<?php e($page->intro()->isNotEmpty(), $page->intro()->text(), $site->description()->text()) ?>">
+<meta property="og:description" content="<?php e($page->text()->isNotEmpty(), $page->text()->excerpt(255), "Un texte de Quentin Armand – Design de Julien Bidoret – Maison des éditions.") ?>">
 <meta property="og:site_name" content="<?= $site->title() ?>">
 <meta property="og:locale" content="<?= $kirby->language() ?>">
 
@@ -32,4 +35,11 @@ $og_cover = $cover->thumb(['width' => 1200, 'height' => 630, 'crop' => true]);
 <meta property="og:image" content="<?= $og_cover->url() ?>">
 <meta property="og:image:width" content="<?= $og_cover->width() ?>">
 <meta property="og:image:height" content="<?= $og_cover->height() ?>">
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@julienbidoret" />
+<meta name="twitter:title" content="<?= $site->title() ?>" />
+<meta name="twitter:description" content="<?php e($page->text()->isNotEmpty(), $page->text()->excerpt(255), "Un texte de Quentin Armand – Design de Julien Bidoret – Maison des éditions.") ?>" />
+<meta name="twitter:image" content="<?= $og_cover->url() ?>" />
+
 
