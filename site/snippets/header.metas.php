ac38bb45b246af838f08b7e8639b42575ffcb580
Julien Bidoret <julienbidoret@gmail.com>
2020-04-24 00:26:43 +0200

    Better og title

--- site/snippets/header.metas.php
+++ site/snippets/header.metas.php
@@ -19,7 +19,7 @@
 
 <meta property="og:url" content="<?= $page->url() ?>">
 <meta property="og:type" content="website">
-<meta property="og:title" content="<?= r($page !== $site->homePage(), $page->title()->html() . ' | ') . $site->title()->html() ?>">
+<meta property="og:title" content="<?= $site->title()->html() . r($page !== $site->homePage(), ' | ' . $page->title()->html() ) ?>">
 <meta property="og:description" content="<?php e($page->text()->isNotEmpty(), $page->text()->excerpt(255), "Un texte de Quentin Armand.") ?>">
 <meta property="og:site_name" content="<?= $site->title() ?>">
 <meta property="og:locale" content="<?= $kirby->language() ?>">
