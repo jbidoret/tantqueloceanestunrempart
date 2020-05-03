8fbe7530bacaf9cec34e26e80c456eebe6df3559
Julien Bidoret <julienbidoret@gmail.com>
2020-05-03 19:36:44 +0200

    Markdown editor

--- site/config/config.php
+++ site/config/config.php
@@ -2,5 +2,20 @@
 
 return [
     'debug'=> true,
+    // markdown
+    'community.markdown-field.buttons'    => ['headlines', 'bold', 'italic', 'divider', 'link','pagelink', 'email', 'file', 'divider', 'ul', 'ol', 'blockquote'],
+    'community.markdown-field.font'       => [
+      'family'  => 'serif',
+      'scaling' => true,
+      'size'    => 'regular',
+    ],
+    'community.markdown-field.query'      => [
+      'pagelink' => null,
+      'images'   => 'page.images',
+      'files'    => 'page.files.filterBy("type", "!=", "image")',
+    ],
+    'community.markdown-field.modals'     => true,
+    'community.markdown-field.blank'      => false,
+    'community.markdown-field.invisibles' => false,
     
 ];
